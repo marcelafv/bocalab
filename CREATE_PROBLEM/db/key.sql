a2d6f4e69f25a80045ff7c6371d0734f9cf7de3a
Marcela Vieira <marcelafvieira@gmail.com>
2020-11-10 00:34:25 -0300

    Set result as Wrong Answer

--- CREATE_PROBLEM/db/key.sql
+++ CREATE_PROBLEM/db/key.sql
@@ -1,5 +1,5 @@
 DELETE FROM customers WHERE id=1;
-INSERT INTO customers (id, name, gender, extra) VALUES (1, "Mario", 2, 4);
+INSERT INTO customers (id, name, gender) VALUES (1, "Mario", 2);
 INSERT INTO customers (id, name, gender) VALUES (2, "Roque", 2);
 UPDATE customers SET name = "Ana", gender = 1 WHERE id=1;
 INSERT INTO customers (id, name, gender) VALUES (8, "Rio", 2);
--- CREATE_PROBLEM/db/professor_key_sample.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM customers WHERE id=1;
-INSERT INTO customers (id, name, gender) VALUES (1, "Mario", 2);
-INSERT INTO customers (id, name, gender) VALUES (2, "Roque", 2);
--- DROP TABLE customers;
