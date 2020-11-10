fceafe21db4457918d10c39e2c6ebf06b96fe1f5
Marcela Vieira <marcelafvieira@gmail.com>
2020-11-10 00:41:25 -0300

    Fix sample files

--- sql_scripts/key_wrong_answer.sql
+++ sql_scripts/key_wrong_answer.sql
@@ -1,4 +1,3 @@
 DELETE FROM customers WHERE id=1;
 INSERT INTO customers (id, name, gender, extra) VALUES (1, "Mario", 2, 4);
 INSERT INTO customers (id, name, gender) VALUES (2, "Roque", 2);
--- DROP TABLE customers;
--- sql_scripts/student_correct.sql
+++ sql_scripts/student_correct.sql
@@ -3,4 +3,3 @@ INSERT INTO customers (id, name, gender) VALUES (5, "Gomez", 2);
 UPDATE customers SET name = "Ana", gender = 1 WHERE id=5;
 INSERT INTO customers (id, name, gender) VALUES (6, "Rita", 1);
 INSERT INTO customers (id, name, gender) VALUES (7, "Alice", 1);
--- CREATE USER 'testtt'@'%' IDENTIFIED BY 'password';
