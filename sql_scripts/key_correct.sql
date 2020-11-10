a40d5cf617917e93afce40e892d5d1b5edd3b611
Marcela Vieira <marcelafvieira@gmail.com>
2020-11-10 00:29:33 -0300

    SQL Scripts and key

---
+++ sql_scripts/key_correct.sql
@@ -0,0 +1,9 @@
+DELETE FROM customers WHERE id=1;
+INSERT INTO customers (id, name, gender) VALUES (1, "Mario", 2);
+INSERT INTO customers (id, name, gender) VALUES (2, "Roque", 2);
+UPDATE customers SET name = "Ana", gender = 1 WHERE id=1;
+INSERT INTO customers (id, name, gender) VALUES (8, "Rio", 2);
+INSERT INTO customers (id, name, gender) VALUES (9, "Mandela", 2);
+INSERT INTO customers (id, name, gender) VALUES (10, "Carol", 1);
+INSERT INTO customers (id, name, gender) VALUES (11, "Camila", 1);
+-- DROP TABLE customers;
---
+++ sql_scripts/student_runtimeerror.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customers (id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, gender TINYINT);
+INSERT INTO customers (id, name, gender) VALUES (5, "Gomez", 2);
+UPDATE customers SET name = "Ana", gender = 1 WHERE id=5;
+INSERT INTO customers (id, name, gender) VALUES (6, "Rita", 1);
+INSERT INTO customers (id, name, gender) VALUES (7, "Alice", 1);
+CREATE USER 'testtt'@'%' IDENTIFIED BY 'password';
